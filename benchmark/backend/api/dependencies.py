dde96d04d3ed67dd4dcc5413893614f35cf0c22b
Alberto Gaona <albertoo_3c@hotmail.com>
2023-08-29 18:02:09 +0000

    fix group size fetch

--- benchmark/backend/api/dependencies.py
+++ benchmark/backend/api/dependencies.py
@@ -6,6 +6,6 @@ from backend.pipelines.models import MODELS_INFO
 
 def llm_dependency(model_name: str) -> Callable[[], GPTQInference]:
     model_dir = MODELS_INFO[model_name]["model_dir"]
-    model_name = MODELS_INFO[model_name]["model_file"]
+    file_name = MODELS_INFO[model_name]["model_file"]
     group_size = MODELS_INFO[model_name]["group_size"]
-    return GPTQInference(model_dir=model_dir, model_name=model_name, group_size=group_size)
+    return GPTQInference(model_dir=model_dir, model_name=file_name, group_size=group_size)
